1236dcabfc113b09e46bc92f617698d3e522fb10
Gibby <git@twoitguys>
2022-03-01 12:10:38 -0500

    Fix getting list of repos

--- .github/docker-hub-pull-all.sh
+++ .github/docker-hub-pull-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS=$(curl -s "https://hub.docker.com/v2/repositories/$DOCKER_USERNAME/" | jq -r '.results | .[] | .name')
+REPOS=$(curl -s "https://hub.docker.com/v2/repositories/$DOCKER_USERNAME/?page_size=1000" | jq -r '.results | .[] | .name')
 
 for REPO in $REPOS; do
   docker pull -a "$DOCKER_USERNAME"/"$REPO"
